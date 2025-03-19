44c5696b505d5917295f54365f7c815b0324fbf2
Jiadong-W <804948890@qq.com>
2025-03-19 22:23:04 +0800

    assignment4

---
+++ assignment4/problem1/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace C_homework4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建一个整数类型的链表
+            GenericList<int> intList = new GenericList<int>();
+            intList.Add(5);
+            intList.Add(7);
+            intList.Add(4);
+            intList.Add(1);
+            intList.Add(2);
+
+            // 打印链表元素
+            Console.WriteLine("链表元素:");
+            intList.ForEach(x => Console.WriteLine(x));
+
+            // 求最大值
+            int max = int.MinValue;
+            intList.ForEach(x => { if (x > max) max = x; });
+            Console.WriteLine($"最大值: {max}");
+
+            // 求最小值
+            int min = int.MaxValue;
+            intList.ForEach(x => { if (x < min) min = x; });
+            Console.WriteLine($"最小值: {min}");
+
+            // 求和
+            int sum = 0;
+            intList.ForEach(x => sum += x);
+            Console.WriteLine($"和: {sum}");
+        }
+    }
+
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)   //ForEach方法
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ assignment4/problem2/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace C_homework4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("闹钟开始！");
+            Clock clock = new Clock();
+            clock.Tick += (sender, e) =>
+            {
+                Console.WriteLine("嘀嗒...");
+            };
+            clock.Alarm += (sender, e) =>
+            {
+                Console.WriteLine("响铃！！！时间到！");
+            };
+
+            // 设置闹钟在 5 秒后响铃
+            int alarmAfterSeconds = 5;
+            Console.WriteLine($"闹钟将在 {alarmAfterSeconds} 秒后响铃。");
+
+            // 启动闹钟
+            clock.Run(alarmAfterSeconds);
+            Console.WriteLine("闹钟模拟结束。");
+        }
+    }
+
+    public class Clock
+    {
+        
+        public event EventHandler Tick;// Tick事件
+        public event EventHandler Alarm;// Alarm事件
+
+        // 闹钟运行方法
+        public void Run(int alarmAfterSeconds)
+        {
+            int seconds = 0;
+
+            while (true)
+            {
+                // 每秒触发一次 Tick 事件
+                OnTick();
+                seconds++;
+
+                // 检查是否到达响铃时间
+                if (seconds >= alarmAfterSeconds)
+                {
+                    OnAlarm(); // 触发 Alarm 事件
+                    break;    
+                }
+
+                Thread.Sleep(1000); // 模拟1秒
+            }
+        }
+
+        // 触发 Tick 事件
+        protected virtual void OnTick()
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+        }
+
+        // 触发 Alarm 事件
+        protected virtual void OnAlarm()
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
